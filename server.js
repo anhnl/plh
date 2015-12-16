18a60821bab70e7b58741f19c81fc27b9845c269
Anh Luong <aluong@imageoptions.net>
2015-12-15 17:22:01 -0800

    First commit

---
+++ server.js
@@ -0,0 +1,36 @@
+// Dependencies
+// -----------------------------------------------------
+var express         = require('express');
+var mongoose        = require('mongoose');
+var morgan          = require('morgan');
+var bodyParser      = require('body-parser');
+var methodOverride  = require('method-override');
+var app             = express();
+var http            = require('http');
+
+// Express Configuration
+// -----------------------------------------------------
+// Sets the connection to MongoDB
+mongoose.connect("mongodb://localhost/MeanMapApp");
+
+// Logging and Parsing
+app.use(express.static(__dirname + '/public'));                 // sets the static files location to public
+app.use('/bower_components',  express.static(__dirname + '/bower_components')); // Use BowerComponents
+app.use(morgan('dev'));                                         // log with Morgan
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.urlencoded({extended: true}));               // parse application/x-www-form-urlencoded
+app.use(bodyParser.text());                                     // allows bodyParser to look at raw text
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));  // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+// Routes
+// ------------------------------------------------------
+require('./app/routes.js')(app);
+
+// Listen
+// -------------------------------------------------------
+app.set('port', process.env.PORT || 8080);
+http.createServer(app).listen(app.get('port'),
+  function(){
+    console.log("Express server listening on port " + app.get('port'));
+});
